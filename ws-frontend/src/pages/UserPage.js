0bc1c83ca9f9c2cc8e333d67cd29cd0a679b6ffa
Ece Basturk <basturkece@gmail.com>
2022-12-16 01:09:05 +0300

    Added UserNotFound Page

--- ws-frontend/src/pages/UserPage.js
+++ ws-frontend/src/pages/UserPage.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProfileCard from '../components/ProfileCard';
+import { getUser } from '../api/apiCalls';
+import { useParams } from 'react-router-dom';
 
 const UserPage = () => {
-    return <div className="container"><ProfileCard /></div>;
+    const [user, setUser] = useState();
+    const [notFound, setNotFound] = useState(false);
+
+    //useParams == props.matchs.params.username
+    const { username } = useParams();
+
+    //user her değiştiğinde useEffect çalışacak
+    useEffect(() => {
+        setNotFound(false);
+    }, [user]);
+
+    useEffect(() => {
+        const loadUser = async () => {
+            try {
+                const response = await getUser(username);
+                setUser(response.data);
+            } catch (error) {
+                setNotFound(true);
+            }
+        };
+
+        loadUser();
+    }, [username]); //username değiştiğinde useEffect çağrılıp güncellenecek
+
+    if (notFound) {
+        return (
+            <div className='container'>
+                <div className='alert alert-danger text-center'>
+                    <div>
+                        <i className="material-icons" style={{ fontSize: '48px' }}>
+                            error
+                        </i>
+                    </div>
+                    {('User Not Found')}
+                </div>
+            </div>
+        )
+    }
+    return (
+        <div className="container">
+            <ProfileCard />
+        </div>
+    );
 };
 
 export default UserPage;

3dd446f9a98bf4ff5b2b42be255763d24454e0bc
Ece Basturk <basturkece@gmail.com>
2022-12-16 20:45:11 +0300

    Profile Card Edit Button

--- ws-frontend/src/components/ProfileCard.js
+++ ws-frontend/src/components/ProfileCard.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
+import Input from './input';
 
 const ProfileCard = props => {
     const { username: loggedInUsername } = useSelector(store => ({ username: store.username }));
     const routeParams = useParams();
+    const [inEditMode, setInEditMode] = useState(false);
 
     const { user } = props;
     const { username, displayName, image } = user;
+
+    const [updatedDisplayName, setUpdatedDisplayName] = useState();
+
+
+
+    useEffect(() => {
+        if (!inEditMode) {
+            setUpdatedDisplayName(undefined);
+        } else {
+            setUpdatedDisplayName(displayName);
+        }
+    }, [inEditMode, displayName]);
+
+    const onClickSave = () => {
+        console.log(updatedDisplayName);
+    };
 
     const pathUsername = routeParams.username;
     let message = 'We cannot edit';
@@ -22,9 +40,39 @@ const ProfileCard = props => {
                 <ProfileImageWithDefault className="rounded-circle shadow" width="200" height="200" alt={`${username} profile`} image={image} />
             </div>
             <div className="card-body">
-                <h3>
-                    {displayName} @{username}
-                </h3>
+                {!inEditMode && (
+                    <>
+                        <h3>
+                            {displayName} @{username}
+                        </h3>
+                        <button className='btn btn-success d-inline-flex' onClick={() => setInEditMode(true)}>
+                            <i className="material-icons">edit</i>
+                            {('Edit')}
+                        </button>
+                    </>
+                )}
+                {inEditMode && (
+                    <div>
+                        <Input
+                            label={('Change Display Name')}
+                            defaultValue={displayName}
+                            onChange={event => {
+                                setUpdatedDisplayName(event.target.value);
+                            }}
+                        />
+                        <div>
+                            <button className="btn btn-primary d-inline-flex me-3" onClick={onClickSave}>
+                                <i className="material-icons ">save</i>
+                                {('Save')}
+                            </button>
+                            <button className="btn btn-dark d-inline-flex" onClick={() => setInEditMode(false)}>
+                                <i className="material-icons">close</i>
+                                {('Cancel')}
+                            </button>
+                        </div>
+                    </div>
+                )}
+
             </div>
         </div>
     );
--- ws-frontend/src/components/input.js
+++ ws-frontend/src/components/input.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Input = (props) => {
-    const { label, error, name, onChange, type } = props;
+    const { label, error, name, onChange, type, defaultValue } = props;
     const className = error ? 'form-control is-invalid' : 'form-control';
     return (
         <div className="mb-3 container">
             <label><b>{label}</b></label>
-            <input name={name} className={className} onChange={onChange} type={type} />
-            <div className="invalid-feedback">{props.error}</div>
+            <input name={name} className={className} onChange={onChange} type={type} defaultValue={defaultValue} />
+            <div className="invalid-feedback">{props.error} </div>
         </div>
     );
 }
